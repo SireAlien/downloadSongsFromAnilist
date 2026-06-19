3b5c039f04d9f2742c25ceafaa06fcbae9806adb
SireAlien <kartoski.karto@gmail.com>
2026-06-19 11:07:39 +0200

    Enhanced song type and language selection
    Added song type selection with checkboxes and language selection with radio buttons. Implemented logic to enable or disable the download button based on user input.

--- ui.py
+++ ui.py
@@ -2,6 +2,7 @@ from tkinter import Tk
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog
+from tkinter import IntVar
 import fetchSongsFromList as ff
 from threading import Thread
 
@@ -13,6 +14,15 @@ def onSelectDirectoryChangeButtonText(directoryButton):
     directory = filedialog.askdirectory()
     if directory:
         directoryButton.config(text=directory)
+        toggleDownloadButtonState()
+
+def toggleDownloadButtonState( ):
+    if op.get() == 1 or ed.get() == 1 or ins.get() == 1:
+        if (RomajRadio.instate(['selected']) or EnglishRadio.instate(['selected'])):
+            if filedialogButton.cget("text") != "Browse":
+                downloadButton.config(state = "normal")
+    else:
+        downloadButton.config(state = "disabled")
 
 def onDownloadButtonClick( ):
     downloadStatusLabel.config(text="Download in progress...")
@@ -22,15 +32,21 @@ def onDownloadButtonClick( ):
 def onDownloadButtonClickThread( ):
     username = usernameEntry.get()
     listName = listEntry.get()
-    songType = songTypeCombo.get()
-    language = languageCombo.get()
+    songTypes = []
+    if OPCheckButton.instate(['selected']):
+        songTypes.append("Opening")
+    if EDCheckButton.instate(['selected']):
+        songTypes.append("Ending")
+    if INCheckButton.instate(['selected']):
+        songTypes.append("Insert")
+    language = "Romaji" if RomajRadio.instate(['selected']) else "English"
     filepath = filedialogButton.cget("text")
 
     if not username or not listName:
         messagebox.showerror("Error", "Please enter both username and list name.")
         return
 
-    ff.downloadMp3FromUsername_List_SongType_Language(username, listName, songType, language, filepath)
+    ff.downloadMp3FromUsername_List_SongType_Language(username, listName, songTypes, language, filepath)
     print("Download completed!")
     downloadStatusLabel.config(text="Download completed!")
 
@@ -41,17 +57,28 @@ ttk.Label(root, text = "List name:").grid(column = 0, row = 1)
 listEntry = ttk.Entry(root, width = 30)
 listEntry.grid(column = 1, row = 1)
 ttk.Label(root, text = "Song type:").grid(column = 0, row = 2)
-songTypeCombo = ttk.Combobox(root, values=["ALL", "OP", "ED", "IN"], state="readonly")
-songTypeCombo.current(0)
-songTypeCombo.grid(column = 1, row = 2)
+songTypeFrame = ttk.Frame(root)
+songTypeFrame.grid(column = 1, row = 2)
+op = IntVar(value=0)
+ed = IntVar(value=0)
+ins = IntVar(value=0)
+OPCheckButton = ttk.Checkbutton(songTypeFrame, text = "OP", command=toggleDownloadButtonState, variable=op)
+EDCheckButton = ttk.Checkbutton(songTypeFrame, text = "ED", command=toggleDownloadButtonState, variable=ed)
+INCheckButton = ttk.Checkbutton(songTypeFrame, text = "IN", command=toggleDownloadButtonState, variable=ins)
+OPCheckButton.grid(column = 0, row = 0, sticky="w")
+EDCheckButton.grid(column = 1, row = 0)
+INCheckButton.grid(column = 2, row = 0, sticky="e")
 ttk.Label(root, text = "Language for filename:").grid(column = 0, row = 3)
-languageCombo = ttk.Combobox(root, values=["JP", "EN"], state="readonly")
-languageCombo.current(0)    
-languageCombo.grid(column = 1, row = 3)
+languageFrame = ttk.Frame(root)
+languageFrame.grid(column = 1, row = 3)
+RomajRadio = ttk.Radiobutton(languageFrame, text = "Romaji", value = "Romaji", command=toggleDownloadButtonState)
+RomajRadio.grid(column = 0, row = 0)
+EnglishRadio = ttk.Radiobutton(languageFrame, text = "English", value = "English", command= toggleDownloadButtonState)
+EnglishRadio.grid(column = 1, row = 0)
 ttk.Label(root, text = "File path to save songs:").grid(column = 0, row = 4)
 filedialogButton = ttk.Button(root, text = "Browse", command = lambda: onSelectDirectoryChangeButtonText(filedialogButton))
 filedialogButton.grid(column = 1, row = 4)
-downloadButton = ttk.Button(root, text = "Download", command = onDownloadButtonClick)
+downloadButton = ttk.Button(root, text = "Download", command = onDownloadButtonClick, state = "disabled")
 downloadButton.grid(column = 0, row = 5)
 downloadStatusLabel = ttk.Label(root, text = "")
 downloadStatusLabel.grid(column = 1, row = 5)
