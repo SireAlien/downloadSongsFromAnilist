1b9951cd7f10d81a577eb76bd9ef5356233fba35
SireAlien <kartoski.karto@gmail.com>
2026-06-19 11:07:05 +0200

    Support multiple song types at once

--- fetchSongsFromList.py
+++ fetchSongsFromList.py
@@ -2,18 +2,18 @@ import fetchList as fl
 import fetchSongs as fs
 
 
-def downloadMp3FromUsername_List_SongType_Language(username, listName, songType, language, path):
+def downloadMp3FromUsername_List_SongTypes_Language(username, listName, songTypes, language, path):
     anime = fl.getAnimeFromUsername_List(username, listName)
     language = f'anime{language}Name'
     for titleEN, titleJP, id in anime:
-        fs.downloadMp3FromTitles_ID_SongType(titleJP, titleEN, [id], fs.songTypes[songType], language, path)
+        fs.downloadMp3FromTitles_ID_SongType(titleJP, titleEN, [id], songTypes, language, path)
 
 if __name__ == "__main__":
     name = input("AniList username: ")
     listName = input("List you want to fetch: ")
-    songType = input("Song type (ALL, OP, ED, IN): ") 
+    songTypes = [fs.songTypes[x] for x in input("Song type (OP, ED, IN): ").split(",")]
     filepath = input("File path to save songs (default is current directory): ") or "./"
     language = input("Language for filename (JP, EN): ")
     language = f'anime{language}Name'
 
-    downloadMp3FromUsername_List_SongType_Language(name, listName, songType, language, filepath)
+    downloadMp3FromUsername_List_SongTypes_Language(name, listName, songTypes, language, filepath)
